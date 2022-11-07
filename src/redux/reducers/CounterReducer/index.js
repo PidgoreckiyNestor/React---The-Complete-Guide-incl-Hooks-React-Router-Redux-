d4c35b4ce22d2117d80905c1a54c0eecbf20abe9
nestor <nestornestorr10@gmail.com>
2022-11-07 23:08:10 +0200

    persist and actions

---
+++ src/redux/reducers/CounterReducer/index.js
@@ -0,0 +1,26 @@
+
+const initialState = {
+  counter: 0,
+};
+
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      if (action.payload){
+        return { counter: state.counter + (action.payload) }
+      }
+      console.log(action);
+      return { counter: state.counter + 1 };
+    case DECREMENT:
+      return { counter: state.counter - 1 };
+  }
+  return state;
+};
+
+export const increment = (value) => ({ type: INCREMENT, payload: value });
+export const decrement = () => ({ type: DECREMENT });
+
+export default counterReducer;
